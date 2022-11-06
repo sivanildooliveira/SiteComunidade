5d7cf2f7de29ac7ef8d9a4f87534eb56ad3d6e69
sivanildooliveira <sivanildoseguro@outlook.com.br>
2022-11-06 15:20:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from comunidadeimpressionadora import app
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(port=port, host='0.0.0.0')
 
